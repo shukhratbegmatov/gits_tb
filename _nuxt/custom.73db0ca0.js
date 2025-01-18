0cc9eedee2143de1f34ed0a517173afdbe7635c1
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2025-01-19 01:08:08 +0500

    fix

---
+++ _nuxt/custom.73db0ca0.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_plugin-vue_export-helper.c27b6911.js";import{o as r,c as s,D as n,q as a}from"./entry.c14770fa.js";const c={};function l(o,e){return r(),s("div",null,[e[0]||(e[0]=n(" Custom layout ")),a(o.$slots,"default")])}const u=t(c,[["render",l]]);export{u as default};
