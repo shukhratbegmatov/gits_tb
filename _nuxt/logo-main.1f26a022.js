480df6af275e0af00453f04a94d66280c961733a
Shuxrat <shuukhratbegmatov@gmail.com>
2025-01-20 11:24:13 +0500

    fix

--- _nuxt/logo-main.1f26a022.js
+++
@@ -1,1 +0,0 @@
-import"./entry.c14770fa.js";const o=""+globalThis.__publicAssetsURL("svg/logo-shape.svg"),_=""+globalThis.__publicAssetsURL("svg/logo-main.svg");export{o as _,_ as a};
---
+++ _nuxt/religions.94e3e7bb.js
@@ -0,0 +1,1 @@
+import{o as e,c as o,b as t,w as s,T as r,a8 as c}from"./entry.e1b59000.js";import{_}from"./_plugin-vue_export-helper.c27b6911.js";const i={name:"religions"},p={class:"w-full"};function d(n,m,f,l,u,x){const a=c;return e(),o("div",p,[t(r,{name:"fade",mode:"out-in"},{default:s(()=>[(e(),o("div",{key:n.$route.path},[t(a)]))]),_:1})])}const g=_(i,[["render",d]]);export{g as default};
