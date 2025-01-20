cf4c315c2973f122aceec55050e3601682126140
Shuxrat <shuukhratbegmatov@gmail.com>
2025-01-20 12:16:39 +0500

    fix

---
+++ _nuxt/composables.b30f66a0.js
@@ -0,0 +1,5 @@
+<<<<<<<< HEAD:_nuxt/composables.b30f66a0.js
+import{a6 as u,a7 as l}from"./entry.e1b59000.js";function m(e,a){const{title:r,titleTemplate:n,...o}=e;return u({title:r,titleTemplate:n,_flatMeta:o},{...a,transform(t){const s=l({...t._flatMeta});return delete t._flatMeta,{...t,meta:s}}})}const M=e=>m(e);export{M as u};
+========
+import{a6 as u,a7 as l}from"./entry.cf20d4e6.js";function m(e,a){const{title:r,titleTemplate:n,...o}=e;return u({title:r,titleTemplate:n,_flatMeta:o},{...a,transform(t){const s=l({...t._flatMeta});return delete t._flatMeta,{...t,meta:s}}})}const M=e=>m(e);export{M as u};
+>>>>>>>> ec4950c (fix):_nuxt/composables.e9c88009.js
